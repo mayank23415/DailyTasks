974b311a9edf12d22e8f3a13710b55e67e188b8d
mayank23415 <52768803+mayank23415@users.noreply.github.com>
2023-03-09 10:56:11 +0530

    Third Commit

--- 07-03-2023/Task - 4/script.js
+++ 07-03-2023/Task - 4/script.js
@@ -1,92 +1,42 @@
-const source = new Map();
-source.set('Delhi', false);
-source.set('Banglore', false);
-source.set('Mumbai', false);
-source.set('Lucknow', false);
-source.set('Punjab', false);
-source.set('Haryana', false);
+let source = document.getElementById("selectSource");
+let destination = document.getElementById("selectDestination");
 
-const destination = new Map();
-destination.set('Delhi', false);
-destination.set('Banglore', false);
-destination.set('Mumbai', false);
-destination.set('Lucknow', false);
-destination.set('Punjab', false);
-destination.set('Haryana', false);
-
-var selectDestination = document.getElementById("selectDestination");
-
-var selectSource = document.getElementById("selectSource");
-for (let city of source.keys()) {
-    var option = document.createElement("option");
-    option.textContent = city;
-    option.value = city;
-    selectSource.appendChild(option);
-}
-
-for (let city of destination.keys()) {
-    var option = document.createElement("option");
-    option.textContent = city;
-    option.value = city;
-    selectDestination.appendChild(option);
-}
-
+let route = new Map();
+route.set("Delhi", ["Delhi"]);
+route.set("Mumbai", ["Mumbai"]);
+route.set("Lucknow", ["Lucknow"]);
+route.set("Punjab", ["Punjab"]);
+route.set("Uttarakhand", ["Uttarakhand"])
 
 function sourceSelection() {
-    console.log(selectSource.value)
-    const opt1 = document.getElementById("selectDestination").getElementsByTagName("option");
-    const opt2 = document.getElementById("selectSource").getElementsByTagName("option");
-    for(let i=0; i<opt2.length; i++) {
-        if(opt2[i].value == selectSource.value){
-            opt2[i].disabled = true;
+    let destOptions = destination.options;
+    let blockedDest = route.get(source.value);
+    //console.log(blockedDest[0]);
+
+    for(let i=0; i<destOptions.length; i++) {
+        destOptions[i].disabled = false;
+    }
+    
+    for(let i=1; i<destOptions.length; i++) {
+        for(let j=0; j<blockedDest.length; j++) {
+            if(destOptions[i].value == blockedDest[j]) {
+                destOptions[i].disabled = true;
+            }
         }
     }
-    for(let i=0; i<opt1.length; i++) {
-        if(opt1[i].value == selectSource.value){
-            opt1[i].disabled = true;
-        }else if(source.get(opt1[i].value) == false){
-            opt1[i].disabled = false;
-        }
-    }
+    
 }
 
-function destinationSelection() {
-    console.log(selectDestination.value)
-    const opt1 = document.getElementById("selectSource").getElementsByTagName("option");
-    const opt2 = document.getElementById("selectDestination").getElementsByTagName("option");
-    for(let i=0; i<opt2.length; i++) {
-        if(opt2[i].value == selectSource.value){
-            opt2[i].disabled = true;
-        }
-    }
-    for(let i=0; i<opt1.length; i++) {
-        if(opt1[i].value == selectSource.value){
-            opt1[i].disabled = true;
-        }else if(source.get(opt1[i].value) == false){
-            opt1[i].disabled = false;
-        }
-    }
-}
+
 
 function main() {
-    source.set(selectSource.value , true)
-    destination.set(selectDestination.value, true);
+    let newBlockedDest = route.get(source.value);
+    newBlockedDest.push(destination.value);
+    route.set(source.value, newBlockedDest);
 
-    const sourceOptions = document.getElementById("selectDestination").getElementsByTagName("option");
-    
-    for(let i=0; i<sourceOptions.length; i++) {
-        if(sourceOptions[i].value == selectSource.value){
-            sourceOptions[i].disabled = true;
-        }
-    }
+    let showRoutes = document.getElementById("routes");
+    showRoutes.innerHTML += source.value + " to " + destination.value + "<br>";
 
-    const destinationOptions = document.getElementById("selectSource").getElementsByTagName("option");
-    for(let i=0; i<destinationOptions.length; i++) {
-        if(destinationOptions[i].value == selectDestination.value){
-            destinationOptions[i].disabled = true;
-        }
-    }
-
-    var routes = document.getElementById("routes");
-    routes.innerHTML += selectSource.value + " to " + selectDestination.value + "<br>";
+    source.selectedIndex = 0;
+    destination.selectedIndex = 0;
 }
